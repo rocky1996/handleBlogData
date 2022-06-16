294bc985b5adfc06189f57fa1a4ed51bc99e994f
rocky1996 <2791752775@qq.com>
2022-06-16 17:41:51 +0800

    初始化项目

---
+++ src/main/java/com/acat/handleblogdata/HandleBlogDataApplication.java
@@ -0,0 +1,13 @@
+package com.acat.handleblogdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleBlogDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandleBlogDataApplication.class, args);
+    }
+
+}
