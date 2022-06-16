1f90a7a8332f1e95f1fca23065a941ed1cb7768a
rocky1996 <2791752775@qq.com>
2022-06-16 17:41:51 +0800

    初始化项目

---
+++ src/test/java/com/acat/handleblogdata/HandleBlogDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acat.handleblogdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleBlogDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
