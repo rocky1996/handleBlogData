be034471efe0b048c8edeafb07ff21c72d0b0287
rocky1996 <2791752775@qq.com>
2022-06-19 17:24:00 +0800

    test

---
+++ src/main/java/com/acat/handleblogdata/enums/RestEnum.java
@@ -0,0 +1,39 @@
+package com.acat.handleblogdata.enums;
+
+public enum RestEnum {
+
+    SUCCESS(0, "成功"),
+    UN_AUTHORIZED(401, "未登录"),
+    FORBIDDEN(402, "没有权限"),
+    SYSTEM_ERROR(402, "系统错误"),
+    NO_HAVING_DATA(403, "没有搜索到任何相关数据"),
+    FAILED(500, "失败"),
+    ;
+
+    private Integer code;
+    private String msg;
+
+    RestEnum() {
+    }
+
+    RestEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
