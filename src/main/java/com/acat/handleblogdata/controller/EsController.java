c9d032da1e979758c24453010e3495ca9285a9cb
rocky1996 <2791752775@qq.com>
2022-06-21 23:01:21 +0800

    test

--- src/main/java/com/acat/handleblogdata/controller/EsController.java
+++ src/main/java/com/acat/handleblogdata/controller/EsController.java
@@ -1,37 +1,37 @@
-package com.acat.handleblogdata.controller;
-
-import com.acat.handleblogdata.service.Article;
-import com.acat.handleblogdata.service.ArticleRepository;
-import com.acat.handleblogdata.constants.RestResult;
-import com.acat.handleblogdata.enums.RestEnum;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
-@Slf4j
-@RestController
-@RequestMapping("/es")
-public class EsController {
-
-    @Autowired
-    private ArticleRepository articleRepository;
-
-    @PostMapping("/save")
-    public RestResult save(@RequestBody Article article) {
-        Article a = articleRepository.save(article);
-        boolean res = a.getId() != null;
-        if (res) {
-            return new RestResult(RestEnum.SUCCESS);
-        }else {
-            return new RestResult(RestEnum.FAILED);
-        }
-    }
-
-    @GetMapping("/findById/{id}")
-    public RestResult findById(@PathVariable String id) {
-        Optional<Article> article = articleRepository.findById(id);
-        return new RestResult(RestEnum.SUCCESS, article);
-    }
-}
+//package com.acat.handleblogdata.controller;
+//
+//import com.acat.handleblogdata.service.Article;
+//import com.acat.handleblogdata.service.ArticleRepository;
+//import com.acat.handleblogdata.constants.RestResult;
+//import com.acat.handleblogdata.enums.RestEnum;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Optional;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/es")
+//public class EsController {
+//
+//    @Autowired
+//    private ArticleRepository articleRepository;
+//
+//    @PostMapping("/save")
+//    public RestResult save(@RequestBody Article article) {
+//        Article a = articleRepository.save(article);
+//        boolean res = a.getId() != null;
+//        if (res) {
+//            return new RestResult(RestEnum.SUCCESS);
+//        }else {
+//            return new RestResult(RestEnum.FAILED);
+//        }
+//    }
+//
+//    @GetMapping("/findById/{id}")
+//    public RestResult findById(@PathVariable String id) {
+//        Optional<Article> article = articleRepository.findById(id);
+//        return new RestResult(RestEnum.SUCCESS, article);
+//    }
+//}
