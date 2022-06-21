4cbe2b33b71785edc956cd948f60b793b6fa83bb
rocky1996 <2791752775@qq.com>
2022-06-21 23:27:14 +0800

    test

--- src/main/java/com/acat/handleBlogData/service/Article.java
+++ src/main/java/com/acat/handleBlogData/service/Article.java
@@ -3,19 +3,19 @@ package com.acat.handleBlogData.service;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-
-import java.util.Date;
-
-@Document(indexName = "article")
-@Data
-@Builder
-public class Article {
-
-    @Id
-    private String id;
-    private String title;
-    private String content;
-    private Integer userId;
-    private Date createTime;
-}
+//import org.springframework.data.elasticsearch.annotations.Document;
+//
+//import java.util.Date;
+//
+//@Document(indexName = "article")
+//@Data
+//@Builder
+//public class Article {
+//
+//    @Id
+//    private String id;
+//    private String title;
+//    private String content;
+//    private Integer userId;
+//    private Date createTime;
+//}
