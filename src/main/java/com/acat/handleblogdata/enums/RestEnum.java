31538a392aee34bd6ff6bf0c857df04be6a4a01b
rocky1996 <2791752775@qq.com>
2022-06-21 11:46:43 +0800

    test

--- src/main/java/com/acat/handleblogdata/enums/RestEnum.java
+++ src/main/java/com/acat/handleblogdata/enums/RestEnum.java
@@ -5,6 +5,7 @@ public enum RestEnum {
     SUCCESS(0, "成功"),
     USERNAME_EMPTY_PARAM(401, "用户名不能为空"),
     PASSWORD_EMPTY_PARAM(402, "密码不能为空"),
+    USER_NOT_EXISTS(403, "该用户不存在"),
     FORBIDDEN(402, "没有权限"),
     SYSTEM_ERROR(402, "系统错误"),
     NO_HAVING_DATA(403, "没有搜索到任何相关数据"),
--- src/main/java/com/acat/handleblogdata/service/impl/UserServiceImpl.java
+++ src/main/java/com/acat/handleblogdata/service/impl/UserServiceImpl.java
@@ -1,16 +1,31 @@
 package com.acat.handleblogdata.service.impl;
 
+import com.acat.handleblogdata.constants.RestResult;
 import com.acat.handleblogdata.controller.bo.LoginReqBo;
+import com.acat.handleblogdata.dao.UserDao;
+import com.acat.handleblogdata.domain.BlogSystemUser;
+import com.acat.handleblogdata.enums.RestEnum;
 import com.acat.handleblogdata.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Resource
+    private UserDao userDao;
+
     @Override
-    public LoginReqBo login(String userName, String password) {
+    public RestResult<LoginReqBo> login(String userName, String password) {
+        BlogSystemUser blogSystemUser = userDao.userLogin(userName, password);
+        if (blogSystemUser == null) {
+            return new RestResult<>(RestEnum.USER_NOT_EXISTS);
+        }
+
+
         return null;
     }
 }
