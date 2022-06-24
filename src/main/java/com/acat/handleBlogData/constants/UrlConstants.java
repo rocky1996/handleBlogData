7acfc336fbf1f0ff379e12b7b0821c69f4a0a0b1
rocky1996 <2791752775@qq.com>
2022-06-24 16:43:59 +0800

    test

--- src/main/java/com/acat/handleBlogData/constants/UrlConstants.java
+++ src/main/java/com/acat/handleBlogData/constants/UrlConstants.java
@@ -1,6 +1,13 @@
 package com.acat.handleBlogData.constants;
 
 public class UrlConstants {
+    /**
+     * 登录
+     */
     public static final String BLOG_SYSTEM_USER_URL = "/blogSystem/user";
+
+    /**
+     * es
+     */
     public static final String BLOG_SYSTEM_ES_URL = "/blogSystem/es";
 }
