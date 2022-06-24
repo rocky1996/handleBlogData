a7538564b8cffda8cfb87fc944d9abdad5a1b18b
rocky1996 <2791752775@qq.com>
2022-06-24 17:08:42 +0800

    test

---
+++ src/main/java/com/acat/handleBlogData/outerService/outerConstants/OutUrlConstants.java
@@ -0,0 +1,7 @@
+package com.acat.handleBlogData.outerService.outerConstants;
+
+public class OutUrlConstants {
+
+    /*************中译语音翻译**************/
+    public static String VIDEO_TRANSLATE_URL = "http://translateport.yeekit.com/translate";
+}
