4ce93403179ecfb89a1f21955aac61d609954ca0
rocky1996 <2791752775@qq.com>
2022-06-24 17:08:42 +0800

    test

---
+++ src/main/java/com/acat/handleBlogData/outerService/outInterface/TranslateOuterServiceImpl.java
@@ -0,0 +1,61 @@
+package com.acat.handleBlogData.outerService.outInterface;
+
+import com.acat.handleBlogData.outerService.outerConstants.OutUrlConstants;
+import com.acat.handleBlogData.util.JacksonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Service
+@Slf4j
+@Component
+public class TranslateOuterServiceImpl {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    private static String ENGLISH_PARAM_KEY = "srcl";
+    private static String CHINESE_PARAM_KEY = "tgtl";
+    private static String CHINESE_PARAM_VALUE = "nzh";
+    private static String TEXT_KEY = "text";
+
+    public String getTranslateValue(String textValue) {
+
+        try {
+            if (StringUtils.isBlank(textValue)) {
+                log.info("TranslateOuterServiceImpl.getTranslateValue,textValue is null");
+                return null;
+            }
+
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            HttpHeaders httpHeader = new HttpHeaders();
+            httpHeader.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
+            paramMap.add(ENGLISH_PARAM_KEY, "");
+            paramMap.add(CHINESE_PARAM_KEY, CHINESE_PARAM_VALUE);
+            paramMap.add(TEXT_KEY, textValue);
+            HttpEntity<MultiValueMap<String, Object>> requestParam = new HttpEntity<>(paramMap, httpHeader);
+            ResponseEntity<String> outerResp = restTemplate.postForEntity(OutUrlConstants.VIDEO_TRANSLATE_URL, requestParam, String.class);
+            stopWatch.stop();
+            log.info("获取中译语音翻译数据,requestParam:{},outerResp:{},time:{}", JacksonUtil.beanToStr(requestParam), JacksonUtil.beanToStr(outerResp), stopWatch.getTotalTimeMillis());
+
+            if (HttpStatus.OK != outerResp.getStatusCode()) {
+                log.info("TranslateOuterServiceImpl.getTranslateValue,translate failed!!!");
+                return null;
+            }
+            return JacksonUtil.beanToStr(outerResp.getBody());
+        }catch (Exception e) {
+            log.error("TranslateOuterServiceImpl.getTranslateValue has error",e.getMessage());
+            return null;
+        }
+    }
+}
