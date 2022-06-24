34893d18644daeea031067333f3fbc5fc3e15fc5
rocky1996 <2791752775@qq.com>
2022-06-24 21:38:02 +0800

    test

--- src/main/java/com/acat/handleBlogData/outerService/outerConstants/OutUrlConstants.java
+++ src/main/java/com/acat/handleBlogData/outerService/outerConstants/OutUrlConstants.java
@@ -3,5 +3,8 @@ package com.acat.handleBlogData.outerService.outerConstants;
 public class OutUrlConstants {
 
     /*************中译语音翻译**************/
-    public static String VIDEO_TRANSLATE_URL = "http://translateport.yeekit.com/translate";
+    public static String TRANSLATE_URL = "http://translateport.yeekit.com/translate";
+
+    /*************语种识别**************/
+    public static String LANGUAGE_IDENTIFY_URL = "http://121.196.186.253:14024/detect";
 }
