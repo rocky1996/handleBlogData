532ee0dce157c216b3257c37761085fe1539eed8
rocky1996 <2791752775@qq.com>
2022-06-26 18:09:33 +0800

    test

--- src/main/java/com/acat/handleBlogData/outerService/outerConstants/OutUrlConstants.java
+++ src/main/java/com/acat/handleBlogData/outerService/outerConstants/OutUrlConstants.java
@@ -5,6 +5,6 @@ public class OutUrlConstants {
     /*************中译语音翻译**************/
     public static String TRANSLATE_URL = "http://translateport.yeekit.com/translate";
 
-    /*************语种识别**************/
-    public static String LANGUAGE_IDENTIFY_URL = "http://121.196.186.253:14024/detect";
+//    /*************语种识别**************/
+//    public static String LANGUAGE_IDENTIFY_URL = "http://121.196.186.253:14024/detect";
 }
--- src/test/java/com/acat/handleBlogData/HandleBlogDataApplicationTests.java
+++ src/test/java/com/acat/handleBlogData/HandleBlogDataApplicationTests.java
@@ -1,13 +1,25 @@
 package com.acat.handleBlogData;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
+@Slf4j
+@Component
 class HandleBlogDataApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+//    @Test
+//    public void testLanguageIdentify() {
+//        String awaitingTranValue = "こんにちは";
+//        Object language = languageIdentifyOuterService.getLanguageIdentifyResult(awaitingTranValue);
+//        log.info("language:{}",language);
+//    }
 }
