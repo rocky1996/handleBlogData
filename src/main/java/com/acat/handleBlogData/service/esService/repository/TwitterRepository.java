d92c583c85a883d92abc959dfebb2a756bb90c99
rocky1996 <2791752775@qq.com>
2022-06-26 22:20:43 +0800

    test

--- src/main/java/com/acat/handleBlogData/service/esService/repository/TwitterRepository.java
+++ src/main/java/com/acat/handleBlogData/service/esService/repository/TwitterRepository.java
@@ -1,7 +1,6 @@
-package com.acat.handleBlogData.service.esService;
+package com.acat.handleBlogData.service.esService.repository;
 
 import com.acat.handleBlogData.domain.esDb.TwitterUserData;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/acat/handleBlogData/util/ReaderFileUtil.java
+++ src/main/java/com/acat/handleBlogData/util/ReaderFileUtil.java
@@ -1,29 +1,63 @@
 package com.acat.handleBlogData.util;
 
+import com.acat.handleBlogData.domain.esDb.TwitterUserData;
+import com.acat.handleBlogData.enums.MediaSourceEnum;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
+import java.util.List;
 
 @Slf4j
 public class ReaderFileUtil {
 
     /**
      * 读取本地文件
-     * @param filePath
+     * @param targetFile
      * @return
      */
-    public static String readFile(String filePath) {
+    public static Object readFile(File targetFile, MediaSourceEnum mediaSourceEnum) {
+
+        List<Object> objList = Lists.newLinkedList();
         try {
-            File targetFile = new File(filePath);
             InputStreamReader rdCto = new InputStreamReader(new FileInputStream(targetFile));
             BufferedReader bfReader = new BufferedReader(rdCto);
             String textLine = null;
             while ((textLine = bfReader.readLine()) != null) {
-                return textLine;
+                if (StringUtils.isNotBlank(textLine)) {
+
+                    switch (mediaSourceEnum) {
+                        case TWITTER:
+                            TwitterUserData twitterUserData = JacksonUtil.strToBean(textLine, TwitterUserData.class);
+                            objList.add(twitterUserData);
+                            break;
+                        case FB_IMPL:
+                            break;
+                        case FB_HISTORY:
+                            break;
+                        case FQ_IMPL:
+                            break;
+                        case FQ_HISTORY:
+                            break;
+                        case INSTAGRAM:
+                            break;
+                        case LINKEDIN_IMPL:
+                            break;
+                        case LINKEDIN_HISTORY:
+                            break;
+                        case LINKEDIN_BUSINESS:
+                            break;
+                        case LINKEDIN_SCHOOL:
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         } catch (IOException e) {
             log.info("ReaderFileUtil.readFile has error:{}",e.getMessage());
         }
-        return null;
+        return objList;
     }
 }
