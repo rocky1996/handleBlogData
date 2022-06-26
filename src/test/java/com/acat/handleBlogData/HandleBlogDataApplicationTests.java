8e4dd25473770629da786cb91314432f6a524d43
rocky1996 <2791752775@qq.com>
2022-06-26 20:23:11 +0800

    添加twitter的es数据

--- src/test/java/com/acat/handleBlogData/HandleBlogDataApplicationTests.java
+++ src/test/java/com/acat/handleBlogData/HandleBlogDataApplicationTests.java
@@ -1,7 +1,13 @@
 package com.acat.handleBlogData;
 
+import com.acat.handleBlogData.domain.esDb.TwitterUserData;
+import com.acat.handleBlogData.service.ArticleRepository;
+import com.acat.handleBlogData.service.esService.TwitterRepository;
+import com.acat.handleBlogData.util.JacksonUtil;
+import com.acat.handleBlogData.util.ReaderFileUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +17,11 @@ import javax.annotation.Resource;
 @Slf4j
 @Component
 class HandleBlogDataApplicationTests {
+
+    @Resource
+    private ArticleRepository articleRepository;
+    @Resource
+    private TwitterRepository twitterRepository;
 
     @Test
     void contextLoads() {
@@ -22,4 +33,41 @@ class HandleBlogDataApplicationTests {
 //        Object language = languageIdentifyOuterService.getLanguageIdentifyResult(awaitingTranValue);
 //        log.info("language:{}",language);
 //    }
+
+//    @Test
+//    public void testArticle() {
+//        Article article = Article.builder()
+//                .id(1)
+//                .title("标题")
+//                .userId(001)
+//                .content("标题标题标题")
+//                .createTime(new Date())
+//                .build();
+//        List<Article> articleList = null;
+//        articleRepository.saveAll(articleList);
+//        Article es = articleRepository.save(article);
+//        boolean res = es.getId() != null;
+//        System.out.println(res);
+//    }
+
+    @Test
+    public void testTwitter() {
+//        String filePath = "D:\\en-001.txt";
+//        String textValue = ReaderFileUtil.reader(filePath);
+//        TwitterUserData twitterUserData = JacksonUtil.strToBean(textValue, TwitterUserData.class);
+//        TwitterUserData es = twitterRepository.save(twitterUserData);
+//        System.out.println(JacksonUtil.beanToStr(es));
+    }
+
+    @Test
+    public void test01() {
+        String filePath = "D:\\en-001.txt";
+        String textValue = ReaderFileUtil.readFile(filePath);
+        if (StringUtils.isNotBlank(textValue)) {
+            TwitterUserData twitterUserData = JacksonUtil.strToBean(textValue, TwitterUserData.class);
+//            System.out.println(JacksonUtil.beanToStr(twitterUserData));
+            TwitterUserData es = twitterRepository.save(twitterUserData);
+            System.out.println(JacksonUtil.beanToStr(es));
+        }
+    }
 }
