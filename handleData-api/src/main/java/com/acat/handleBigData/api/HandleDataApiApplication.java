aac7c27f62d3062a74666126e4f48057049b9efd
rocky1996 <2791752775@qq.com>
2022-06-27 12:34:39 +0800

    第二次拆分补追加

---
+++ handleData-api/src/main/java/com/acat/handleBigData/api/HandleDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.acat.handleBigData.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandleDataApiApplication.class, args);
+    }
+
+}
