6c706bacb45e899631850dfcb736affd73df02b1
rocky1996 <2791752775@qq.com>
2022-06-27 10:05:43 +0800

    test

---
+++ handleData-api/main/java/com/acat/handleBlogData/Hello.java
@@ -0,0 +1,4 @@
+package main.java.com.acat.handleBlogData;
+
+public class Hello {
+}
---
+++ handleData-api/main/java/com/acat/handleBlogData/constants/UrlConstants.java
@@ -0,0 +1,9 @@
+package main.java.com.acat.handleBlogData.constants;
+
+public class UrlConstants {
+
+    /**
+     * 对外接口
+     */
+    public static final String BLOG_SYSTEM_OPEN_API_URL = "/blogSystem/openApi/";
+}
