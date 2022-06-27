033bb65a31faeaa97982bee4da515485c20b560f
rocky1996 <2791752775@qq.com>
2022-06-27 14:39:07 +0800

    test

--- handleData-core/main/java/com/acat/handleBlogData/service/esService/repository/FqImplRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acat.handleBlogData.service.esService.repository;
-
-import com.acat.handleBlogData.domain.esDb.FqUserImplData;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FqImplRepository extends ElasticsearchRepository<FqUserImplData, String> {
-}
