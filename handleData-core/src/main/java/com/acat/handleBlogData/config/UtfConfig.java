f4f8147550e1a6fe491e9b175307f137006e70e3
rocky1996 <2791752775@qq.com>
2022-08-11 11:59:14 +0800

    test

---
+++ handleData-core/src/main/java/com/acat/handleBlogData/config/UtfConfig.java
@@ -0,0 +1,19 @@
+package com.acat.handleBlogData.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class UtfConfig {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("utf-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
+}
